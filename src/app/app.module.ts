5083b8df5945a14cd712c80f7285fbc584a1d3bb
charatkosit <charatkosit@gmail.com>
2024-06-27 21:59:38 +0700

    rev0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TdkExampleComponent } from './tdk-example/tdk-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TdkExampleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/websocket.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+  private wSocket: WebSocket | null = null;
+  private debugFlag = true;
+
+  private messageSubject = new Subject<any>();
+  public messages$ = this.messageSubject.asObservable();
+
+  constructor() {}
+
+  connect(url: string): void {
+    this.wSocket = new WebSocket(url);
+    this.wSocket.onopen = () => {
+      if (this.debugFlag) {
+        console.log("WebSocket: onopen() event called.");
+      }
+    };
+
+    this.wSocket.onmessage = (evt) => {
+      this.messageSubject.next(evt.data);
+    };
+
+    this.wSocket.onclose = () => {
+      this.handleConnectionError("Agent: No connection");
+    };
+
+    this.wSocket.onerror = () => {
+      this.handleConnectionError("Agent: No connection");
+    };
+  }
+
+  sendMessage(message: any): void {
+    if (this.wSocket) {
+      if (this.debugFlag) {
+        console.log(">" + JSON.parse(message).Command);
+      }
+      this.wSocket.send(message);
+    }
+  }
+
+  closeConnection(): void {
+    if (this.wSocket) {
+      this.wSocket.close();
+    }
+  }
+
+  private handleConnectionError(errorMessage: string): void {
+    this.messageSubject.next({ error: errorMessage });
+    if (this.debugFlag) {
+      console.log("WebSocket error: ", errorMessage);
+    }
+  }
+}
